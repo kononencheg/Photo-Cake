3bc88463d7b9df77ec3d42f95b28fac779b7648d
Sergey Kononenko <kononencheg@gmail.com>
2011-12-05 00:18:11 +0400

    post to wall problem

--- application/api/social/vk/wall/Post.php
+++ application/api/social/vk/wall/Post.php
@@ -21,19 +21,30 @@ class Post extends \api\social\vk\VKMethod {
      * @return mixed
      */
     protected function apply() {
-
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_VERBOSE, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $this->upload_url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
-            "photo" => "@" . $this->_file,
-        ));
 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+        curl_setopt($ch, CURLOPT_URL, $this->upload_url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+        curl_setopt($ch, CURLOPT_REFERER, $_SERVER['HTTP_REFERER']);
+        curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+            "photo" => '@' . $this->_file,
+        ));
+        
         $result = curl_exec($ch);
+        $error = curl_error($ch);
 
         var_dump($result);
-        return $result;
+        
+        return null;
+
+
     }
 }
+
