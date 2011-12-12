6c9323fb48b3dedd8cace994965aac73b95765e5
Sergey Kononenko <kononencheg@gmail.com>
2011-12-12 23:44:51 +0400

    advanced dummy

--- public/index.php
+++ public/index.php
@@ -29,6 +29,8 @@ if (isset($_GET['i'])){
         <script type="text/javascript" src="http://yandex.st/share/share.js"></script>
 
         <script>
+            var DECO_DATA = '{"weightsList":[1,1.5,2,2.5,3,3.5,4,4.5,5],"ratiosList":[0.6,0.55,0.5,0.45,0.4,0.38,0.32,0.3,0.25],"personsList":[6,10,15,20,25,30,35,40,45],"decoSelectors":[{"deco":[{"url":"/img/app/deco/cherry.png","autorotate":true,"name":"cherry","description":"Вишня"},{"url":"/img/app/deco/grape.png","autorotate":true,"name":"grape","description":"Виноград"},{"url":"/img/app/deco/kiwi.png","autorotate":true,"name":"kiwi","description":"Киви"},{"url":"/img/app/deco/raspberry.png","autorotate":true,"name":"raspberry","description":"Малина"},{"url":"/img/app/deco/strawberry.png","autorotate":true,"name":"strawberry","description":"Клубника"},{"url":"/img/app/deco/orange.png","autorotate":true,"name":"orange","description":"Апельсин"},{"url":"/img/app/deco/peach.png","autorotate":true,"name":"peach","description":"Персик"},{"url":"/img/app/deco/lemon.png","autorotate":true,"name":"lemon","description":"Лимон"}]},{"deco":[{"url":"/img/app/deco/pig1.png","autorotate":false,"name":"pig1","description":"Сахарная фигурка"},{"url":"/img/app/deco/car1.png","autorotate":false,"name":"car1","description":"Сахарная фигурка"},{"url":"/img/app/deco/hare1.png","autorotate":false,"name":"hare1","description":"Сахарная фигурка"},{"url":"/img/app/deco/hedgehog1.png","autorotate":false,"name":"hedgehog1","description":"Сахарная фигурка"},{"url":"/img/app/deco/moose1.png","autorotate":false,"name":"moose1","description":"Сахарная фигурка"},{"url":"/img/app/deco/owl1.png","autorotate":false,"name":"owl1","description":"Сахарная фигурка"},{"url":"/img/app/deco/pin1.png","autorotate":false,"name":"pin1","description":"Сахарная фигурка"},{"url":"/img/app/deco/sheep1.png","autorotate":false,"name":"sheep1","description":"Сахарная фигурка"},{"url":"/img/app/deco/raven1.png","autorotate":false,"name":"raven1","description":"Сахарная фигурка"},{"url":"/img/app/deco/bear1.png","autorotate":false,"name":"bear1","description":"Сахарная фигурка"},{"url":"/img/app/deco/car2.png","autorotate":false,"name":"car2","description":"Сахарная фигурка"},{"url":"/img/app/deco/car3.png","autorotate":false,"name":"car3","description":"Сахарная фигурка"},{"url":"/img/app/deco/mat1.png","autorotate":false,"name":"mat1","description":"Сахарная фигурка"},{"url":"/img/app/deco/doll1.png","autorotate":false,"name":"doll1","description":"Сахарная фигурка"},{"url":"/img/app/deco/doll2.png","autorotate":false,"name":"doll2","description":"Сахарная фигурка"}]},{"deco":[{"url":"/img/app/deco/flower1.png","autorotate":false,"name":"flower1","description":"Сахарная фигурка"},{"url":"/img/app/deco/flower2.png","autorotate":false,"name":"flower2","description":"Сахарная фигурка"},{"url":"/img/app/deco/flower3.png","autorotate":false,"name":"flower3","description":"Сахарная фигурка"},{"url":"/img/app/deco/flower4.png","autorotate":false,"name":"flower4","description":"Сахарная фигурка"},{"url":"/img/app/deco/flower5.png","autorotate":false,"name":"flower5","description":"Сахарная фигурка"},{"url":"/img/app/deco/flower6.png","autorotate":false,"name":"flower6","description":"Сахарная фигурка"}]}]}';
+
             function $(id) { return document.getElementById(id); }
 
             function show(id) { $(id).style.display = 'block'; }
@@ -39,7 +41,7 @@ if (isset($_GET['i'])){
 
             function onFlashReady() {
                 cakeDesigner = swfobject.getObjectById('cake_designer');
-                cakeDesigner.initialize('round', 0.75);
+                cakeDesigner.initialize(DECO_DATA, 'round', 0.75);
             }
 
             function saveCakeData(junk, imageData) {
@@ -105,7 +107,7 @@ if (isset($_GET['i'])){
                 </div>
 
                 <div class="logo">
-                    <img alt="Фотонаторте" src="/img/logo.png" />
+                    <img alt="Фотонаторте" src="/img/app/logo.png" />
                     <div class="logo-subtitle">Скоро открытие! А пока...</div>
                 </div>
             </div>
@@ -138,19 +140,20 @@ if (isset($_GET['i'])){
                             <input class="submit-email" type="submit" value="Хочу &raquo;"/>
                         </p>
                     </form>
+                    <p>Кондитерским: <a href="#">условия сотрудничества</a>.</p>
                 </div>
                 <div class="center-column">
                     <h2>Делаем торт!</h2>
-                    <object id="cake_designer" width="660" height="600"
+                    <object id="cake_designer" width="660" height="665"
                             classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">
 
-                        <param name="movie" value="/swf/cake-designer-fake.swf" />
+                        <param name="movie" value="/swf/cake-designer.swf" />
                         <param name="allowscriptaccess" value="always" />
                         <param name="wmode" value="opaque" />
 
                         <!--[if !IE]>-->
-                        <object width="660" height="600" wmode="opaque"
-                                data="/swf/cake-designer-fake.swf"
+                        <object width="660" height="665" wmode="opaque"
+                                data="/swf/cake-designer.swf"
                                 type="application/x-shockwave-flash"
                                 allowscriptaccess="always">
                         <!--<![endif]-->
