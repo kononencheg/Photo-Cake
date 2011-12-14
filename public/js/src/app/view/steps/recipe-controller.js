e3f8b67ea3cc76f1e40d80b12489c290d4f6b7f3
Sergey Kononenko <kononencheg@gmail.com>
2011-12-15 01:14:14 +0400

    recipe selection

--- public/js/src/app/view/steps/recipe-controller.js
+++ public/js/src/app/view/steps/recipe-controller.js
@@ -4,6 +4,9 @@
         tuna.view.StepViewController.call(this, id);
 
         this.__cakeImage = null;
+        this.__descriptionPopup = null;
+
+        this.__popupRecipe = null;
     };
 
     tuna.extend(RecipeController, tuna.view.StepViewController);
@@ -18,8 +21,6 @@
         for (var i in created) {
             this._container.initModules(created[i]);
         }
-
-        debugger;
     };
 
     RecipeController.prototype._bootstrap = function() {
@@ -31,12 +32,61 @@
     };
 
     RecipeController.prototype._initActions = function() {
+        this.__initRecipeList();
+        this.__initRecipeSelection();
+        this.__initDescriptionPopup();
+
         this.__cakeImage = tuna.dom.selectOne('#recipe_cake_image');
 
         this._db.subscribe('cake_params', this.__updateView, this);
         this._db.subscribe('cake_image_element', this.__updateView, this);
 
         this.__updateView();
+    };
+
+    RecipeController.prototype.__initRecipeList = function() {
+        this._db.set('recipes', [
+            { 'id': 1, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' },
+            { 'id': 2, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' },
+            { 'id': 3, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' },
+            { 'id': 4, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' }
+        ]);
+    };
+
+    RecipeController.prototype.__initDescriptionPopup = function() {
+        this.__descriptionPopup = ui.Popup.create
+                            (tuna.dom.selectOne('#recipe_description_popup'));
+
+        this.__descriptionPopup.subscribe('popup-apply', function() {
+            var input = tuna.dom.selectOne(
+                'input[value=' + this.__popupRecipe.index + '].j-recipe-radio'
+            );
+
+            input.checked = true;
+            tuna.dom.dispatchEvent(input, 'change');
+        }, this);
+    };
+
+    RecipeController.prototype.__initRecipeSelection = function() {
+        var self = this;
+
+        tuna.dom.addChildEventListener(
+            this._target, '.j-recipe-image', 'click', function(event) {
+                var index = this.getAttribute('data-index');
+                var recipes  = self._db.get('recipes');
+                
+                self.__popupRecipe = recipes[index];
+                self.__popupRecipe.index = index;
+                self.__updateView();
+            }
+        );
+
+        tuna.dom.addChildEventListener(
+            this._target, 'input.j-recipe-radio', 'change',
+            function(event) {
+                alert(this.value);
+            }
+        );
     };
 
     RecipeController.prototype.__updateView = function() {
@@ -50,12 +100,8 @@
                     'persons_count': cakeParams.dimensions.persons_count,
                     'price': price + ' рублей'
                 },
-                'recipes': [
-                    { 'id': 1, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' },
-                    { 'id': 2, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' },
-                    { 'id': 3, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' },
-                    { 'id': 4, 'name': 'Тортильон кононенски', 'image_url': '/img/app/recipes/bayliss.jpg' }
-                ]
+                'recipes': this._db.get('recipes'),
+                'popup_recipe': this.__popupRecipe
             });
         }
 
