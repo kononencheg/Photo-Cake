bcc43547e257258df16bb6661731fe68220817b8
Sergey Kononenko <kononencheg@gmail.com>
2011-12-23 19:19:32 +0400

    capistrano settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,19 +9,17 @@
 
 set :application, "Photo-Cake" 
 
+set :scm, :git
+set :scm_verbose, true
+
 set :repository,  "git@github.com:kononencheg/#{application}.git"
 
-set :scm, :git
-
-set :scm_verbose, true
-set :ssh_options, { :forward_agent => true }
-
 default_run_options[:pty] = true
-
-set :branch, "master"
 
 role :app, "178.250.241.76"
 role :web, "178.250.241.76"
 role :db,  "178.250.241.76", :primary => true
 
+set :ssh_options, { :forward_agent => true }
 set :use_sudo, false
+
