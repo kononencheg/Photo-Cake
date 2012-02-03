113c4a1a18260024ac0872ac89ff710d8e3cbbeb
Sergey Kononenko <kononencheg@gmail.com>
2012-02-03 19:17:56 +0400

    remove tuna

--- public/js/lib/tuna/tmpl/compilers/i-item-compiler.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-    var IItemCompiler = function() {};
-
-    IItemCompiler.prototype.compile
-        = function(element, templateSettings, template) {};
-
-    tuna.tmpl.compilers.IItemCompiler = IItemCompiler;
-})();
--- public/js/lib/tuna/tmpl/units/i-list-item-router.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-    var IListItemRouter = function() {};
-    
-    IListItemRouter.prototype.append = function(element) {};
-
-    tuna.tmpl.units.IListItemRouter = IListItemRouter;
-})();
